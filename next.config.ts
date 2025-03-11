581e82a39b6bcf756d6e62467f32c4491c04623b
pasinduvinsuka <nimsarapasindu1@gmail.com>
2025-03-11 23:55:05 +0530

    next config update

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  basePath: "/home",
   images: {
     domains: ['images.unsplash.com'],
   },
